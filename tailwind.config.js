3dcfb9d33c8d29676b2796dfac2d6ecd30f67aad
Amit Yadav <vy9172631@gmail.com>
2025-04-12 20:21:37 +0530

    new

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}',  // Ensure this path includes `app/` directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
